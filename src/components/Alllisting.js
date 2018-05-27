59c870f7263f50269bdee2bc6af44ffd1faebcc0
Abdulrahman <almaddadalhussein85@gmail.com>
2018-05-27 03:18:28 -0700

    nothing

--- src/components/Alllisting.js
+++ src/components/Alllisting.js
@@ -1,52 +1,34 @@
-//Allposts.js
+//Alllisting.js
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
-
-function Result(props) {
-    return (
-        props.posts.map((post)=>{
-            return(
-                <ul className='users' key={post._id}>
-                <div className="card-body">
-          
-                    <h2 className="card-title">Name: {post.name}</h2>
-            
-
-              
-                    <hr />
-                </div>
-                </ul>
-            )
-        })
-    )
-}
-
 class Alllisting extends Component {
     constructor(props) {
         super(props);
         this.state={
-            posts:null
+            data:null,
+        
         }
+        axios.get(`api/getAllListings/`)
+        .then((data)=>{this.setState({data:data.data})});
         
+      
     }
-    componentDidMount(){
-        axios.get('http://localhost:3001/api/getAllListings/')
-        .then((res)=>this.setState({posts:res.data})) 
-        
-    }
-  
-
- 
-
-    render() {
-        return (
-            <div>
-            {this.state.posts && <Result posts ={this.state.posts} />}
     
-             </div>
+    render() {
+        var listing = this.state.data;
+        return (
+      
+            (this.state.data &&
+            <div className="card text-center">
+                <div className="card-body">
+                
+                    <p className="card-text"><b>Description:</b> {listing}</p>
+                    <p className="card-text btn-sm"> <b>Location:</b> {listing}</p>
+                </div>
+            </div>)
         );
     }
 }
